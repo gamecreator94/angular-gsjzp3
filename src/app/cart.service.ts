3bb4c70ae47eebb9deed4e55e003438cecdfee0c
gamecreator94 <rawsonmb@gmail.com>
2021-07-22 08:41:55 -0400

    4 files modified

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -6,4 +6,17 @@ import { Product } from './products';
 export class CartService {
   items: Product[] = [];
   constructor() {}
+
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
